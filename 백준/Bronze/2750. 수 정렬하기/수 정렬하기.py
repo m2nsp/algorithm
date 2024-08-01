216a24165efaef7d9d525dc5f2ebd5536e0861a0
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-01 18:01:44 +0900

    [Bronze II] Title: 수 정렬하기, Time: 140 ms, Memory: 110284 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,14 +1,9 @@
-import heapq
-
 N = int(input())
 A = [0]*N
 for i in range(N):
     A[i] = int(input())
-min_heap = []
 
-for i in range(N):
-    heapq.heappush(min_heap, A[i])
+A.sort()
 
-# 힙에서 하나씩 꺼내어 출력
-while min_heap:
-    print(heapq.heappop(min_heap), end=' ')
+for item in A:
+    print(item, end=' ')
