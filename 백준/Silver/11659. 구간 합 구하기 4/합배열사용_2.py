2faa7596f6bd840ad04de3dd4810758f62cedd3c
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-01 23:26:02 +0900

    Update 합배열사용_2.py

--- 백준/Silver/11659. 구간 합 구하기 4/합배열사용_2.py
+++ 백준/Silver/11659. 구간 합 구하기 4/합배열사용_2.py
@@ -6,8 +6,8 @@ for i in range(1, N+1):
     S.append(S[i-1]+A[i-1])
 
 for _ in range(M):
-    j, k = map(int, input().split())
-    ans = S[k]-S[j-1]
-    print(ans)
+    j, k = map(int, input().split())         # 각 질의의 구간 읽어옴
+    ans = S[k]-S[j-1]                        # 각 질의의 답
+    print(ans)                               # 답 출력
 
 # 인덱스 오류 조심하자!! 0부터 시작이니까!
