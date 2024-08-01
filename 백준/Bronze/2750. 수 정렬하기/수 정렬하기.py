d2465e8d4d64607d8f312cac5a79e69200efad06
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-01 18:03:52 +0900

    Update 수 정렬하기.py

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,9 +1,9 @@
-N = int(input())
-A = [0]*N
+N = int(input())                                        # 첫 번째 줄의 수의 개수 N 읽어옴
+A = [0]*N                                               # N개의 숫자를 저장할 배열 초기화
 for i in range(N):
-    A[i] = int(input())
+    A[i] = int(input())                                 # N개의 숫자 저장
 
-A.sort()
+A.sort()                                                # A 오름차순 정렬
 
-for item in A:
+for item in A:                                          # 앞에서 부터 A 출력
     print(item, end=' ')
