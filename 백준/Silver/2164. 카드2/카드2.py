030eb082bdccc6d02c36e2eac2d63f152665989e
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-02 16:46:34 +0900

    Update 카드2.py

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,11 +1,11 @@
-from collections import deque                                                   # 파이썬에서는 큐를 deque로 구현
-N = int(input())                                                                # 카드의 개수 입력받기
-myQueue = deque()                                                               # deque 생성
+from collections import deque                                             # 파이썬에서는 큐를 deque로 구현***
+N = int(input())                                                          # N = 카드의 개수
+Q = deque()                                                               # deque 생성
 
-for i in range(1, N+1):                                                         # 카드의 개수만큼 반복
-    myQueue.append(i)                                                           # 큐에 카드 저장
-while len(myQueue)>1:                                                           # 큐에 카드가 1장 남을 때까지
-    myQueue.popleft()                                                           # 맨위의 카드를 버림
-    myQueue.append(myQueue.popleft())                                           # 맨 위의 카드를 가장 아래 카드 밑으로 이동
+for i in range(1, N+1):                                                   # 카드의 개수만큼 반복
+    Q.append(i)                                                           # 큐에 카드 저장
+while len(Q)>1:                                                           # 큐에 카드가 1장 남을 때까지 반복문 실행
+    Q.popleft()                                                           # 맨위의 카드를 버림
+    Q.append(Q.popleft())                                                 # 맨 위의 카드를 가장 아래 카드 밑으로 이동
 
-print(myQueue[0])                                                               # 마지막으로 남은 카드 출력
+print(Q[0])                                                               # 마지막으로 남은 카드 출력
