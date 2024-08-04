d466714fdcbcd72059785490120a1d4134d1ad68
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-04 22:15:42 +0900

    2주차_sorted()함수이용

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,10 +1,10 @@
-N = int(input())
+N = int(input())                                        # N : 수의 개수
 
-A = []
+A = []                                                  # 주어진 수들 저장할 리스트 초기화
 for _ in range(N):
     A.append(int(input()))
 
-sorted_A = sorted(A)
+sorted_A = sorted(A)                                    # 오름차순으로 정렬
 
 for i in range(N):
     print(sorted_A[i])
