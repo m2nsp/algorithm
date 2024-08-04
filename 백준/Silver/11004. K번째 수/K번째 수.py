8c6a8c07e34ac20c73cdc2f941ab8e8985d677cb
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-04 22:13:40 +0900

    2주차_sorted함수이용

--- 백준/Silver/11004. K번째 수/K번째 수.py
+++ 백준/Silver/11004. K번째 수/K번째 수.py
@@ -1,7 +1,7 @@
-N, K = map(int, input().split())
+N, K = map(int, input().split())                          # N : 주어진 수의 개수 / K : 구하려는 수의 순서
+  
+A = list(map(int, input().split()))                       # A : 주어진 수들 저장하는 리스트
 
-A = list(map(int, input().split()))
-
-sorted_A = sorted(A)
+sorted_A = sorted(A)                                      # sorted_A : A를 오름차순으로 정렬한 결과 저장
 
 print(sorted_A[K-1])
