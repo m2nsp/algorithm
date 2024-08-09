9d953e5f2c98279369dfc121ab154a1ce678a4a3
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-09 16:10:37 +0900

    2주차_dfs

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -3,8 +3,8 @@ sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 N, M = map(int, input().split())        # N : 캠프 참가 인원, M : 친구 관계의 수
 arrive = False                          # 도착 했는지 확인하는 변수 초기화
-A = [[]for _ in range(N+1)]
-visited = [False]*(N+1)
+A = [[]for _ in range(N+1)]             # 인접 리스트 생성
+visited = [False]*(N+1)                 # 방문 기록 저장 : default=False
 
 def dfs(now, depth):
     global arrive
@@ -19,7 +19,7 @@ def dfs(now, depth):
     
 for _ in range(M):
     u, v = map(int, input().split())
-    A[u].append(v)
+    A[u].append(v)                                # 친구 관계이므로 방향이 없는 그래프
     A[v].append(u)
     
 for i in range(N):
