8331941cdf7c033e3191405e3e14512d3abb2938
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-10 23:56:53 +0900

    3주차_그리디

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,13 +1,13 @@
-A = list(map(str, input().split("-")))
+A = list(map(str, input().split("-")))                                    # A: 주어진 문자열 -를 기준으로 쪼개서 받아옴
 
-total_sum = 0
+total_sum = 0                                                             # 최종 합
 
 for i in range(len(A)):
-    B = A[i].split("+")
+    B = A[i].split("+")                                                   # 쪼개진 요소들을 다시 +를 기준으로 나눔
     for item in B:
-        if i == 0:
+        if i == 0:                                                        # 첫 요소는 더함
             total_sum += int(item)
-        else:
+        else:                                                             # 나머지 요소는 뺌
             total_sum -= int(item)
 
 print(total_sum)
