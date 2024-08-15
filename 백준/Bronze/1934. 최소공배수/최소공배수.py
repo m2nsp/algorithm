452471c851a71b444f1251430d9035818e2136d4
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-16 03:56:44 +0900

    3주차_최소공배수.py

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,12 +1,12 @@
-def gcd(A, B):
+def gcd(A, B):                        # 최대 공약수 구하는 함수 (재귀 이용)
     if B == 0:
         return A
     else:
         return gcd(B, A % B)
 
-N = int(input())                        #테스트 케이스 개수
+N = int(input())                      # 테스트 케이스 개수
 
 for _ in range(N):
     A, B = map(int, input().split())
-    result = A * B // gcd(A, B)  
+    result = A * B // gcd(A, B)       # 수학적인 성질에 따라 -- 최소 공배수는 두 수의 곱을 그들의 최대 공약수로 나누어 구할 수 있음
     print(result)
