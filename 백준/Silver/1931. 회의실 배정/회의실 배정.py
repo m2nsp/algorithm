84c39a86b647ca17058077a20217a293da1dc603
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-15 16:48:35 +0900

    Update 회의실 배정.py

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -4,12 +4,12 @@ N = int(input())                            # N : 회의 수
 A = [[0]*2 for _ in range(N)]               # A : 주어진 회의들 저장할 배열
 
 for i in range(N):
-    u, v = map(int, input().split())
+    u, v = map(int, input().split())        # 시작, 종료시간을 각각 받아옴
     A[i][0] = v
     A[i][1] = u
     
-A.sort()                                    # 종료 시간이 빠른 순으로 정렬됨
-cnt = 0
+A.sort()                                    # 종료 시간이 빠른 순으로 정렬됨  -- 종료 시간을 0번 인덱스에 미리 저장함 -- sort()함수는 기본적으로 0번 인덱스를 기준으로 정렬을 수행함
+cnt = 0                                     # 진행할 수 있는 회의의 수 저장하는 변수
 end = -1                                    # 종료 시각 나타내는 변수
 
 for i in range(N):
