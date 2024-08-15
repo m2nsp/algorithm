b22794c19fd3ab99fb18a29f04f35a30a215399f
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-16 02:53:34 +0900

    3주차_제곱ㄴㄴ수

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -2,20 +2,18 @@ import sys
 import math
 
 input = sys.stdin.readline
-min_value, max_value = map(int, input().split())
+min, max = map(int, input().split())                                # 최솟값, 최댓값 읽어오기
 
-S = []  # 제곱수들 저장할 변수
-for i in range(2, int(math.sqrt(max_value)) + 1):
+S = []                                                              # 제곱수들 저장할 리스트
+for i in range(2, int(math.sqrt(max)) + 1):
     S.append(i * i)
+ 
+A = [1] * (max - min + 1)                                           # A 리스트는 min_value에서 max_value까지의 범위를 다룸
 
-# A 리스트는 min_value에서 max_value까지의 범위를 다룸
-A = [1] * (max_value - min_value + 1)
-
-# 제곱수의 배수들을 0으로 설정
-for item in S:
-    start = max(item, (min_value + item - 1) // item * item)
-    for i in range(start, max_value + 1, item):
-        A[i - min_value] = 0
+for item in S:                                                      # 제곱수의 배수들을 0으로 설정
+    start = max(item, ((min + item - 1) // item) * item)            # min 보다 커아하는 조건
+    for i in range(start, max + 1, item):
+        A[i - min] = 0
 
 # 남아있는 1의 개수를 카운트
 cnt = 0
