842534edbce97d5fdee14a3f28cdfc359407f293
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-16 01:32:44 +0900

    3주차_소수＆팰린드롬.py

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -1,5 +1,5 @@
 import math
-n = int(input())
+n = int(input())                                            # 주어진 수 n
 
 def is_prime(n):                                            # n이 소수인지 확인하는 함수
     if n < 2:
@@ -9,11 +9,11 @@ def is_prime(n):                                            # n이 소수인지 
             return False
     return True
 
-def is_pelindrom(n):
-    n  = str(n)
+def is_pelindrom(n):                                        # 펠린드롬인지 확인하는 함수
+    n  = str(n)                                             # 숫자로는 슬라이싱 이용할 수 없음 => 문자열, 튜플, 리스트와 같은 자료형에만 사용가능
     return n == n[::-1]
 
-num = n
+num = n                                                     # n이상의 값이므로 n으로 초기화
 
 while True:
     if (is_prime(num) and is_pelindrom(num)):
