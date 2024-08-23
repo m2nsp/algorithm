06cebc464f33412528e4fb9f5117c9d04a12f8ec
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-23 14:37:34 +0900

    4주차_게임 개발.py

--- 백준/Gold/1516. 게임 개발/게임 개발.py
+++ 백준/Gold/1516. 게임 개발/게임 개발.py
@@ -1,31 +1,31 @@
 from collections import deque
 
-N = int(input())                                            # N : 
-A = [[] for _ in range(N+1)]                                # A : 
-indegree = [0]*(N+1)
-selfBuild = [0]*(N+1)
+N = int(input())                                            # N : 건물 수
+A = [[] for _ in range(N+1)]                                # A : 건물의 선행 건물 저장
+indegree = [0]*(N+1)                                        # 진입 차수 저장
+selfBuild = [0]*(N+1)                                       # 짓는 데 걸리는 시간 저장
 
-for i in range(1, N+1):
+for i in range(1, N+1):                                     # 건물별 건설 시간 및 선행 작업 입력
     inputList = list(map(int, input().split()))
-    selfBuild[i] = inputList[0]
+    selfBuild[i] = inputList[0]                             # 건물 i의 건설 시간
     index = 1
     while True:
         preTemp = inputList[index]
         index += 1
-        if preTemp == -1:
+        if preTemp == -1:                                   
             break
-        A[preTemp].append(i)
-        indegree[i] += 1
+        A[preTemp].append(i)                               # 선행 건물이 완료된 후에야 건물 i 건설 가능
+        indegree[i] += 1                                   # 건물 i의 진입 차수 증가
         
-queue = deque()
+queue = deque()                                            # 위상정렬 -- 큐 생성 
 
-for i in range(1, N+1):
+for i in range(1, N+1):                                    # 진입 차수가 0인 건물 삽입
     if indegree[i] == 0:
         queue.append(i)
         
-result = [0]*(N+1)
+result = [0]*(N+1)                                         # 각 건물을 완성하는 데 걸리는 최종 시간 저장
 
-while queue:
+while queue:                                               # 위상 정렬 알고리즘 수행
     now = queue.popleft()
     for next in A[now]:  
         indegree[next] -= 1
@@ -33,5 +33,5 @@ while queue:
         if indegree[next] == 0: 
             queue.append(next)
             
-for i in range(1, N+1):
+for i in range(1, N+1):                                    # 결과 출력
     print(result[i] + selfBuild[i])
