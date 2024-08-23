b233f403531da1a62bcf2c8320217c79955a3015
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-23 14:25:52 +0900

    4주차_줄 세우기.py

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,19 +1,19 @@
 from collections import deque
-N, M = map(int, input().split())                        # N: , M: 
-A = [[]for _ in range(N+1)]
-indegree = [0]*(N+1)
+N, M = map(int, input().split())                        # N: 노드 개수 , M: 간선 개수 
+A = [[]for _ in range(N+1)]                             # A: 각 노드에 연결된 노드들 저장 리스트
+indegree = [0]*(N+1)                                    # 각 노드의 진입 차수 저장
 
-for i in range(M):
+for i in range(M):                                      # 간선 정보 저장
     S, E = map(int, input().split())
     A[S].append(E)
     indegree[E]+=1
-queue=deque()
+queue=deque()                                           # 큐 생성
 
 for i in range(1, N+1):
-    if indegree[i]==0:
+    if indegree[i]==0:                                  # 진입 차수가 0인 노드 큐에 삽입
         queue.append(i)
         
-while queue:
+while queue:                                            # 위상 정렬 수행
     now = queue.popleft()
     print(now, end=' ')
     for next in A[now]:
