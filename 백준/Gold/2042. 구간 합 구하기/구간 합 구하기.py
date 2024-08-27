e989d4f2148007942f8f76abc0e5e59aca85db4c
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-27 11:33:29 +0900

    [Gold I] Title: 구간 합 구하기, Time: 588 ms, Memory: 154632 KB -BaekjoonHub

---
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -0,0 +1,54 @@
+import sys
+input = sys.stdin.readline
+
+#세그먼트 트리 생성
+def build_segment_tree(arr, seg_tree, node, start, end):
+    if start == end:
+        seg_tree[node] = arr[start]
+    else:
+        mid = (start+end)//2
+        build_segment_tree(arr, seg_tree, 2*node, start, mid)
+        build_segment_tree(arr, seg_tree, 2*node+1, mid+1, end)
+        seg_tree[node] = seg_tree[2*node] + seg_tree[2*node+1]
+
+# 구간 합 구하기
+def range_sum(seg_tree, node, start, end, left, right):
+    if right < start or end < left:
+        return 0
+    if left <=start and end <= right:
+        return seg_tree[node]
+    mid = (start+end)//2
+    l_sum = range_sum(seg_tree, 2*node, start, mid, left, right)
+    r_sum = range_sum(seg_tree, 2*node+1, mid+1, end, left, right)
+    return l_sum + r_sum
+
+# 값 업데이트
+def update(seg_tree, node, start, end, idx, value):
+    if start == end:
+        seg_tree[node] = value
+    else:
+        mid = (start+end)//2
+        if start <= idx <= mid:
+            update(seg_tree, 2*node, start, mid, idx, value)
+        else:
+            update(seg_tree, 2*node+1, mid+1, end, idx, value)
+        seg_tree[node] = seg_tree[2*node] + seg_tree[2*node+1]
+
+#입력 받기
+N, M, K = map(int, input().split())
+A = [int(input()) for _ in range(N)]
+
+#세그먼트 트리 초기화
+seg_tree = [0]*(4*N)        # 세그먼트 트리는 완전 이진 트리일 경우 높이가 log2(N) --> 이는 2*N에서 4*N 사이임
+build_segment_tree(A, seg_tree, 1, 0, N-1)
+
+#처리
+for _ in range(M+K):
+    a, b, c  = map(int, input().split())
+    # 값 변경
+    if a==1:
+        update(seg_tree, 1, 0, N-1, b-1, c)
+    # 구간 합 출력
+    else:
+        print(range_sum(seg_tree, 1, 0, N-1, b-1, c-1))
+                
