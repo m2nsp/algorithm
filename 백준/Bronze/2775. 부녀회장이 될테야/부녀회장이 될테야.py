50165d84a0b1e138fe1b65bdc65db1ad883ad670
m2nsp <129494557+m2nsp@users.noreply.github.com>
2024-08-30 14:54:48 +0900

    5주차_부녀회장이 될테야.py

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,18 +1,21 @@
 import sys
 input = sys.stdin.readline
-D = [[0 for j in range(15)] for i in range(15)]
+D = [[0 for j in range(15)] for i in range(15)]        # DP 테이블 생성
 
+# DP테이블 초기화
 for i in range(1, 15):
     D[i][1] = 1
     D[0][i] = i
     
 for i in range(1, 15):
     for j in range(2, 15):
-        D[i][j] = D[i][j-1] + D[i-1][j]
+        D[i][j] = D[i][j-1] + D[i-1][j]        # 조합 점화식
         
-T = int(input())
+T = int(input())    # T: 질의의 수
 
+
+# 질의 구하기
 for i in range(0, T):
-    K = int(input())
+    K = int(input())    # K:층 N:호
     N = int(input())
     print(D[K][N])
