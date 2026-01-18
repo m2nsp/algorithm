c69a29732b452a3a45fbbee60a299d7f0105df35
m2nsp <129494557+m2nsp@users.noreply.github.com>
2026-01-18 12:40:53 +0900

    [Silver IV] Title: ATM, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,22 +1,11 @@
 N = int(input())
-A = list(map(int, input().split()))
+P = list(map(int, input().split()))
 
-S = []
-for i in range(N):
-    S.append((i, A[i]))
+S = [0]*N # 합배열
+SP = sorted(P)
 
-# A[i]를 기준으로 정렬
-S_sorted = sorted(S, key=lambda x: x[1])
+S[0] = SP[0]
+for i in range(1, N):
+    S[i] = S[i-1] + SP[i]
 
-ans = [0]*N
-for i in range(N):
-    if i==0:
-        ans[i] = S_sorted[i][1]
-    else:
-        ans[i] += S_sorted[i][1] + ans[i-1]
-
-Ans = 0
-for i in range(N):
-    Ans += ans[i]
-
-print(Ans)
+print(sum(S))
