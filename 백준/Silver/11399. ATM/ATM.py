c6a2564e8f7b03ba83d4772da63fec5fb4315948
m2nsp <129494557+m2nsp@users.noreply.github.com>
2026-01-18 11:09:51 +0900

    [Silver IV] Title: ATM, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,21 +1,21 @@
-N = int(input())                                                    # N : 사람 수
-A = list(map(int, input().split()))                                 # 각 사람의 주어진 인출 시간
+N = int(input())
+A = list(map(int, input().split()))
 
-S = []                                                              # 인덱스와 인출시간 동시에 저장하는 배열
+S = []
 for i in range(N):
     S.append((i, A[i]))
 
 # A[i]를 기준으로 정렬
 S_sorted = sorted(S, key=lambda x: x[1])
 
-ans = [0]*N                                                        # 각 사람의 총 걸리는 시간을 계산해서 저장하는 합 배열
+ans = [0]*N
 for i in range(N):
     if i==0:
         ans[i] = S_sorted[i][1]
     else:
         ans[i] += S_sorted[i][1] + ans[i-1]
 
-Ans = 0                                                             # 총 인출시간을 저장하는 변수
+Ans = 0
 for i in range(N):
     Ans += ans[i]
 
