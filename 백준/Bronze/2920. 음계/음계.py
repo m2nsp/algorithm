78ccdaff410387f7a0d7194703dad25e0e5b55d7
m2nsp <129494557+m2nsp@users.noreply.github.com>
2026-01-21 16:00:20 +0900

    [Bronze II] Title: 음계, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,17 +1,8 @@
 arr = list(map(int, input().split()))
-if arr[0] == 1:
-    for i in range(1, 8):
-        if arr[i] != arr[i-1] +1:
-            ans = "mixed"
-            break
-        ans = "ascending"
-elif arr[0] == 8:
-    for i in range(1, 8):
-        if arr[i-1] -1 != arr[i]:
-            ans = "mixed"
-            break
-        ans = "descending"
+
+if arr == list(range(1, 9)):
+    print("ascending")
+elif arr == list(range(8, 0, -1)):
+    print("descending")
 else:
-    ans = "mixed"
-    
-print(ans)
+    print("mixed")
